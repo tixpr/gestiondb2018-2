7bdb17d320d0445ed6e167c5598da77c4ea18684
alex chilquillo <kirito99alex@gmail.com>
2018-09-28 13:39:06 -0500

    trabajo

--- database/migrations/2018_09_28_181544_create_mentors_table.php
+++ database/migrations/2018_09_28_181544_create_mentors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMentorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mentors', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email',68)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',45);
-            $table->string('username',200);      
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('pp_projections_id');
+            $table->foreign('pp_projections_id')->references('id')->on('pp_projections');
             $table->timestamps();
-            $table->boolean('is_valid')->default(false);
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mentors');
     }
 }
