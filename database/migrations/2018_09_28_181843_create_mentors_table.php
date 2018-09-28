c74491c3f6a0fd84b55e94296004c57b1bda34b2
Jean Perez Hilario <jean_higua@hotmail.com>
2018-09-28 14:03:48 -0500

    migracion de todas las tablas

---
+++ database/migrations/2018_09_28_181843_create_mentors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMentorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentors', function (Blueprint $table) {
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mentors');
+    }
+}
