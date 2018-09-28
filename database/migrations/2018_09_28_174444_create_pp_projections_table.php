9f5c7a66a61f9d3b441e7bc31fa9262d76032b44
paul <pulpo8464@gmail.com>
2018-09-28 13:38:45 -0500

    trabajo

---
+++ database/migrations/2018_09_28_174444_create_pp_projections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePpProjectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pp_projections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',200);
+            $table->string('organization,200');
+            $table->timestamps();
+            $table->date('exist_at');
+            $table->boolean('is_exit')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pp_projections');
+    }
+}
