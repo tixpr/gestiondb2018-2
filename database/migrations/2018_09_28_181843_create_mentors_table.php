b070036c74aedcff4fab95321631a7dd9529499e
Jean Perez Hilario <jean_higua@hotmail.com>
2018-09-28 14:19:17 -0500

    todas las tablas creadas

--- database/migrations/2018_09_28_181843_create_mentors_table.php
+++ database/migrations/2018_09_28_181843_create_mentors_table.php
@@ -14,7 +14,12 @@ class CreateMentorsTable extends Migration
     public function up()
     {
         Schema::create('mentors', function (Blueprint $table) {
-            
+            $table->bigIncrements('id');
+			$table->date('created_at');
+            $table->unsignedBigInteger('user_id');
+			$table->unsignedBigInteger('pp_projection_id');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('pp_projection_id')->references('id')->on('pp_projections');
         });
     }
 
