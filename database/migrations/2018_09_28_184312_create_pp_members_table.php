7f0d2163bba3bf3537c91324d8244ee27f52d873
Adderlyn Tito Palacios Rojas <www.adderlyn@gmail.com>
2018-09-28 13:48:55 -0500

    todas las migraciones realizadas

---
+++ database/migrations/2018_09_28_184312_create_pp_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePpMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pp_members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+			$table->unsignedBigInteger('pp_projection_id');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('pp_projection_id')->references('id')->on('pp_projections');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pp_members');
+    }
+}
