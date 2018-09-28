ab03ecd2a48b7adc7b94e24fa07ff1ea6063dcd8
Eliana Soto Pacheco <lianjsp@gmail.com>
2018-09-28 14:24:32 -0500

    ya subi todas las tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->bigIncrements('id');
+            $table->string('username',200);
+            $table->string('email',60)->unique();
+            $table->string('password',45);
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_valid')->default(false);
         });
     }
 
