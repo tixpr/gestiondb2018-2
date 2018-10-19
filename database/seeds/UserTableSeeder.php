bc0a4e014daa00bac512d995beed713300edee28
Hugher Barja Carhuaz <hugherbarja@gmail.com>
2018-10-19 12:42:41 -0500

    Creacion de una semilla

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+     {
+        DB::table('users')->insert([
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'username' => str_random(15),
+            'is_valid' => true
+        ]);
+    }
+}
