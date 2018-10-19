1ccab666768818f480ff3ad10dc8651a1fc3dcd8
Ever Canales Requena <lanced2014@gmail.com>
2018-10-19 14:29:06 -0500

    practica 19-10-2018

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+        $this->call(ppprojectionstableseeder::class);
+        $this->call(ppmemberstableseedereseeder::class);
+        $this->call(ppmentorstableseedertableseeder::class);
+        $this->call(ppreportstableseeder::class);
+        $this->call(ppreviserstableseedertableseeder::class);
     }
 }
