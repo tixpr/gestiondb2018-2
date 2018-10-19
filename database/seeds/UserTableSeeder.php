55fb2faf2ec0f8b3ed2fd0da8cbd6408a2a84372
karhenSV <karhen.solano.v@gmail.com>
2018-10-19 12:33:20 -0500

    guardar

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,14 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name'          => str_random(10),
+            'email'         => str_random(10).'@gmail.com',
+            'password'      => bcrypt('secret'),
+            'create_at'      => now(),
+            'updated_at'      => now(),
+            'username'      => str_random(15),
+            'is_Valid'      => true,
+        ]);
     }
 }
