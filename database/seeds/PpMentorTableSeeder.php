7a47dd9aa519b3dcbd9eb80ec3f83f50d732b8d4
karhenSV <karhen.solano.v@gmail.com>
2018-10-24 19:28:11 -0500

    guardar

---
+++ database/seeds/PpMentorTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PpMentorTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
