23c64331b888a642a746c8ff9b5120e5de3e0526
karhenSV <karhen.solano.v@gmail.com>
2018-10-26 12:40:03 -0500

    todos los modelos

---
+++ app/Models/PpMentor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PpMentor extends Model
+{
+        /**
+         * The table associated with the model.
+         *
+         * @var string
+         */
+        protected $table = 'pp_mentors';
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'updated_at'=false,
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id',
+    ];
+}
