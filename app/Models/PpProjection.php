1ad966ee21bac2917562be3e52a00957e995164d
Adderlyn Tito Palacios Rojas <www.adderlyn@gmail.com>
2018-10-26 18:05:34 -0500

    clase del dia 26-10-2018

---
+++ app/Models/PpProjection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PpProjection extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+	protected $table = 'pp_projections';
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+		'title',
+		'organization',
+		'exit_at',
+		'is_exit',
+		'is_practice',
+		'user_id'
+	];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id'
+    ];
+}
