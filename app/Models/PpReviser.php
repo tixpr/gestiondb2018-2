ad90542820f70a154df64948fe4efd4545df4b5f
paul <pulpo8464@gmail.com>
2018-10-26 12:38:46 -0500

    practica-concultas

---
+++ app/Models/PpReviser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PpReviser extends Model
+{
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pp_revisers';
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+       
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id' 
+    ];
+}
