009b0808d332815f2a38b17845797d2bfb2dd4cb
Carlo López Balbín <carlolb1232@gmail.com>
2018-10-26 18:29:45 -0500

    practica 26-10-18

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::namespace('Api')->group(function(){
     Route::prefix('user')->group(function(){
         Route::get('/','UserController@index');
         Route::post('/','UserController@store');
+        
         Route::get('{id}','UserController@show');
         Route::put('{id}','UserController@update');
         Route::delete('{id}','UserController@destroy');
