f5578a7ebb5d8eff96c502523d82f208793fc6fd
karhenSV <karhen.solano.v@gmail.com>
2018-10-26 18:15:54 -0500

    clase 26-10-18

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,40 @@ Route::namespace('Api')->group(function(){
         Route::put('{id}','UserController@update');
         Route::delete('{id}','UserController@destroy');
     });
-
+    Route::prefix('user')->group(function(){
+        Route::get('/','PpProjectionController@index');
+        Route::post('/','PpProjectionController@store');
+        Route::get('{id}','PpProjectionController@show');
+        Route::put('{id}','PpProjectionController@update');
+        Route::delete('{id}','PpProjectionController@destroy');
+    });
+    Route::prefix('user')->group(function(){
+        Route::get('/','PpMemberController@index');
+        Route::post('/','PpMemberController@store');
+        Route::get('{id}','PpMemberController@show');
+        Route::put('{id}','PpMemberController@update');
+        Route::delete('{id}','PpMemberController@destroy');
+    });
+    Route::prefix('user')->group(function(){
+        Route::get('/','PpMentorController@index');
+        Route::post('/','PpMentorController@store');
+        Route::get('{id}','PpMentorController@show');
+        Route::put('{id}','PpMentorController@update');
+        Route::delete('{id}','PpMentorController@destroy');
+    });
+    Route::prefix('user')->group(function(){
+        Route::get('/','PpReportController@index');
+        Route::post('/','PpReportController@store');
+        Route::get('{id}','PpReportController@show');
+        Route::put('{id}','PpReportController@update');
+        Route::delete('{id}','PpReportController@destroy');
+    });
+    Route::prefix('user')->group(function(){
+        Route::get('/','PpReviserController@index');
+        Route::post('/','PpReviserController@store');
+        Route::get('{id}','PpReviserController@show');
+        Route::put('{id}','PpReviserController@update');
+        Route::delete('{id}','PpReviserController@destroy');
+    });
 });
 
