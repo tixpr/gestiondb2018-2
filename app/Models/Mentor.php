7e1e5d4abef28c9dde2813de547e7b66f4610c63
Bryan Francis Carbajal Calero <crysaor.andexca01@gmail.com>
2018-11-04 14:03:23 -0500

    Models

---
+++ app/Models/Mentor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+     protected $table='mentors';
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'create_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id'
+    ];
+}
