cdb11b7b02c147e27d18f5a2dc7adb49545e50b3
Bryan Francis Carbajal Calero <crysaor.andexca01@gmail.com>
2018-11-09 11:46:33 -0500

    prueba

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,48 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::namespace('Api')->group(function(){
+    Route::prefix('user')->group(function(){
+    Route::get('/','UserController@index');
+    Route::post('/','UserController@store');
+    Route::get('{id}','UserController@show');
+    Route::put('{id}','UserController@update');
+    Route::delete('{id}','UserController@destroy');
+});
+Route::prefix('member')->group(function(){
+    Route::get('/','PpMemberController@index');
+    Route::post('/','PpMemberController@store');
+    Route::get('{id}','PpMemberController@show');
+    Route::put('{id}','PpMemberController@update');
+    Route::delete('{id}','PpMemberController@destroy');
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('mentor')->group(function(){
+    Route::get('/','PpMentorController@index');
+    Route::post('/','PpMentorController@store');
+    Route::get('{id}','PpMentorController@show');
+    Route::put('{id}','PpMentorController@update');
+    Route::delete('{id}','PpMentorController@destroy');
+});
+Route::prefix('projection')->group(function(){
+    Route::get('/','PpProjectionController@index');
+    Route::post('/','PpProjectionController@store');
+    Route::get('{id}','PpProjectionController@show');
+    Route::put('{id}','PpProjectionController@update');
+    Route::delete('{id}','PpProjectionController@destroy');
+});
+Route::prefix('report')->group(function(){
+    Route::get('/','PpReportController@index');
+    Route::post('/','PpReportController@store');
+    Route::get('{id}','PpReportController@show');
+    Route::put('{id}','PpReportController@update');
+    Route::delete('{id}','PpReportController@destroy');
+});
+Route::prefix('reviser')->group(function(){
+    Route::get('/','PpReviserController@index');
+    Route::post('/','PpReviserController@store');
+    Route::get('{id}','PpReviserController@show');
+    Route::put('{id}','PpReviserController@update');
+    Route::delete('{id}','PpReviserController@destroy');
+});
 });
