54bd6deffa3bc26041ef57184f8d086e2b757699
Bryan Francis Carbajal Calero <crysaor.andexca01@gmail.com>
2018-11-09 13:31:36 -0500

    resource

---
+++ PpReportTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PpReportTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {      
+        for($i=1;$i<=100;$i++){
+            DB::table('pp_reports')->insert([
+            'type'=> random_int(1,3),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'pp_projection_id'=> random_int(1,100)
+        ]);
+
+        }
+    }
+}
